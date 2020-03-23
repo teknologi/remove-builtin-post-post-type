38b0f5801cc9d77876ec1176e96209b43f1e4749
Hans Czajkowski Jørgensen <hans.c.jorgensen@gmail.com>
2020-03-23 15:09:15 +0100

    Add license preamble

--- remove-builtin-post-post-type.php
+++ remove-builtin-post-post-type.php
@@ -8,8 +8,22 @@
  * Author: Hans Czajkowski Jørgensen
  * Text Domain: teknologi_reove_builtin_post_post_type
  * Domain Path: /languages
- */
+ * License:     GPL2
 
+ * Remove builtin Post post type is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 2 of the
+ * License, or any later version.
+*
+ * Remove builtin Post post type is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Remove builtin Post post type. If not, see
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html.
+*/
 namespace Teknologi;
 
 /**
